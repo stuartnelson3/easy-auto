fa5259c63fdd91f5c20d2f74fc357adc7e7c7a62
Stuart Nelson <stuartnelson3@gmail.com>
2013-08-03 08:55:56 -0500

    test to check #set_remote in create-repo

--- test/lib/easy_auto/create_repo_test.rb
+++ test/lib/easy_auto/create_repo_test.rb
@@ -2,28 +2,50 @@ require_relative '../../test_helper'
 
 Client = Class.new do
   def login
-    'test@example.com'
+    'test-user@example.com'
   end
 end
 
-class CreateRepoMock < EasyAuto::CreateRepo
+CreateRepoMock = Class.new(EasyAuto::CreateRepo) do
+  def initialize mock
+    @mock = mock
+  end
+
+  def git
+    @mock
+  end
+
   def client
     Client.new
+  end
+
+  def repo_name
+    'test-repo'
   end
 end
 
 describe EasyAuto::CreateRepo do
-
-  subject { CreateRepoMock.new }
+  before do
+    @mock = MiniTest::Mock.new
+  end
+  subject { CreateRepoMock.new @mock }
 
   it 'gets the username from the client' do
-    subject.client.login.must_equal 'test@example.com'
+    subject.client.login.must_equal 'test-user@example.com'
   end
 
   it 'gets the user name correctly' do
-    subject.username.must_equal 'test'
+    subject.username.must_equal 'test-user'
   end
 
-  # it 'sends the right cli_send messages in #set_remote' do
-  # end
+  it 'sends the right cli_send messages in #set_remote' do
+    username = 'test-user'
+    repo_name = 'test-repo'
+    @mock.expect :perform, true, ["add -A"]
+    @mock.expect :perform, true, ["commit -m 'first commit'"]
+    @mock.expect :perform, true, ["remote add origin git@github.com:#{username}/#{repo_name}.git"]
+    @mock.expect :perform, true, ["push -u origin master"]
+    subject.set_remote
+    @mock.verify
+  end
 end
